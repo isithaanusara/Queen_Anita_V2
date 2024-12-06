3902375b9bd38478e3d8986565f985511a29f2a0
Isitha anuss <isithaanusara01@gmail.com>
2024-12-06 15:59:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaeRru3AD
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/17c8ba84a7761eed633f6.jpg";
 global.devs = "https://t.me/deecee_x";
 global.sudo = process.env.SUDO || "";
-global.owner = process.env.OWNER_NUMBER || "";
+global.owner = process.env.OWNER_NUMBER || "94740152669";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
@@ -42,7 +42,7 @@ global.scan = "https://secret-garden-43998-4daad95d4561.herokuapp.com/";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  ""
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiZ0IzSldiOUxMSXEwMEpleUJXZ3V5V0I0a080RnpuNWs1RDJLSXZ6Z3NFWT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiWWVvYmRxRTU0dkwvK2FMb0F0cVRFQTloTVl5TEtFdTBReG5tM3lWVDlRZz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ5RWdLWXhBLzJVUHRrZDAvb0VlbEduU0lYSEVUbndDRklsdmJkNk9QTVhNPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJyTEczcW0zS2NOc3ZGZ00rbXYyZm11cWd3dG5zMnlFSmdYUm1nckVIQVNFPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkdBSXhwclh2UDhtSGVISklmMTJMMU45blphN2F3d0tYR3JVa1FRN0x3bnc9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjM5Y2NQSG5RbklUOWpzS0RxajJEMXAvaitPV1FsVTAyVThsTzhtZGV4Qmc9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiRUVyQzBhdXB5R0t6Q1R6djRmUWxkbXFqa3NSTVJ2eDFNTTNjYWxlM1ZXZz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiR080amlQUlk5OVE2MUNPYXhRZnZzUEVUNTV0ZWlrUU9sZUQ4Nk54TlYwbz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImdiYWZsZmR6QTVQT21kdkFrNGdlWVNjV3piM2twWlJzNEdJa2xvVkZURzdvalhwZU5NNjFNMW80R0hSckc0azhnbXN6Y0RiRGJ6SW1NQnhLOUR4b2hnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTYxLCJhZHZTZWNyZXRLZXkiOiJ0UzVxZllyb2lDOU5sejBQQXY3eWRDOUZuUEdWblZTZW5IRXBZMTJCOEdrPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJjazNpaWNwQ1JLMlB2YlE3VTM0ZlZBIiwicGhvbmVJZCI6IjVmYjFhYzdlLWYzMDAtNDg0Zi1hMTVlLTc2OWQ2ZGZmOTNmNyIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ6SVlZSVZsVFBHNlV0WXg1MCs1Qmx5RGxaSXM9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiRXRGaFgrYzU5akdFYU9qeXlUOGlOU01hMHFjPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IlgxRVhTVDVKIiwibWUiOnsiaWQiOiI5NDc0MDE1MjY2OToyNEBzLndoYXRzYXBwLm5ldCIsIm5hbWUiOiJJU0hJWUEgTU9EWiJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDUHVRczhJR0VQNml5N29HR0FjZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoicmhWTWhvSlFCakhvOVR5cmh2WE1qU1F1YjYvQmJweGEzRWJRdmhabllURT0iLCJhY2NvdW50U2lnbmF0dXJlIjoib3pxajZQbStTZll4b3E3V0NkZUZHWEhNcFkrU1U2Zm8xNStkWmN3eE9NV2QwTTVLZHMwYlFlRHZ4Z3lNa2hGdWgxcGswamY4MkpOcFdELzF4dTF6QUE9PSIsImRldmljZVNpZ25hdHVyZSI6Ik5CVWVLdHQxQkovY2pXQ2VWaVptWDhyaVdSRkd1dEpOYXRkWURsSjFNTklMZld4M1Q1cm9sd010aTJNempnU2xtQm1WazNMbSt2ZGtVVXBpZDR2eGpRPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiOTQ3NDAxNTI2Njk6MjRAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCYTRWVElhQ1VBWXg2UFU4cTRiMXpJMGtMbSt2d1c2Y1d0eEcwTDRXWjJFeCJ9fV0sInBsYXRmb3JtIjoic21iYSIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTczMzQ4MDg0NSwibXlBcHBTdGF0ZUtleUlkIjoiQUFBQUFBT1YifQ=="
 module.exports = {
   menu: process.env.MENU || "2",
   HANDLERS: process.env.PREFIX || ".",
@@ -52,7 +52,7 @@ module.exports = {
   author: process.env.PACK_AUTHER || "QUEEN_ANITA-V2",
   packname: process.env.PACK_NAME || "A N I T A",
   botname: process.env.BOT_NAME || "QUEEN_ANITA-V2",
-  ownername: process.env.OWNER_NAME || "David Cyril",
+  ownername: process.env.OWNER_NAME || "Isitha Anusara",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
